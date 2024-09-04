7bdfb5ee8a711599e99aab127cd9c1cc196abbc9
kim080120 <s24056@gsm.hs.kr>
2024-09-04 23:12:45 +0900

    김명현

--- main.dart
+++ main.dart
@@ -8,89 +8,113 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              // Top Section
-              Padding(
-                padding: const EdgeInsets.all(20.0),
+      home: MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "",
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  SizedBox(height: 100),
+                  Text(
+                    "쉽고 빠르게\n기숙사 입소 관리",
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  SizedBox(height: 100),
+                  Text(
+                    "김명현님 안녕하세요",
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  SizedBox(height: 5),
+                  Text(
+                    "14m 03s",
+                    style: TextStyle(
+                      fontSize: 45,
+                      color: Colors.green,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 40),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20),
+                  ),
+                ),
                 child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text(
-                      "",
-                      style: TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.w700,
-                      ),
+                    Spacer(),
+                    MenuItem(
+                      color: Colors.green,
+                      text: "기숙사 입소 확인하기",
+                      icon: Icons.meeting_room,
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => GreenScreen()),
+                        );
+                      },
                     ),
-                    SizedBox(height: 100), //하얀색 크기11
-                    Text(
-                      "쉽고 빠르게\n기숙사 입소 관리",
-                      style: TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.w700,
-                      ),
+                    Spacer(),
+                    MenuItem(
+                      color: Colors.orange,
+                      text: "기숙사 미입소 확인하기",
+                      icon: Icons.cancel,
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => OrangeScreen()),
+                        );
+                      },
                     ),
-                    SizedBox(height: 100), //하얀색 크기11
-                    Text(
-                      "김명현님 안녕하세요",
-                      style: TextStyle(
-                        fontSize: 25,
-                        fontWeight: FontWeight.w700,
-                      ),
+                    MenuItem(
+                      color: Colors.blue,
+                      text: "공지사항 확인하기",
+                      icon: Icons.announcement,
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => BlueScreen()),
+                        );
+                      },
                     ),
-                    SizedBox(height: 5), //하얀색 크기11
-                    Text(
-                      "14m 03s",
-                      style: TextStyle(
-                        fontSize: 45,
-                        color: Colors.green,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    SizedBox(height: 40), //검은색 크기 22
+                    SizedBox(height: 10),
                   ],
                 ),
               ),
-              Expanded(
-                child: Container(
-                  padding: EdgeInsets.symmetric(horizontal: 10),
-                  decoration: BoxDecoration(
-                    color: Colors.black,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(20),
-                      topRight: Radius.circular(20),
-                    ),
-                  ),
-                  child: Column(
-                    children: [
-                      Spacer(), // Pushes the green box towards the bottom
-                      MenuItem(
-                        color: Colors.green,
-                        text: "기숙사 입소 확인하기",
-                        icon: Icons.meeting_room,
-                      ),
-                      Spacer(), // Adds space between the green box and the other buttons
-                      MenuItem(
-                        color: Colors.orange,
-                        text: "기숙사 미입소 확인하기",
-                        icon: Icons.cancel,
-                      ),
-                      MenuItem(
-                        color: Colors.blue,
-                        text: "공지사항 확인하기",
-                        icon: Icons.announcement,
-                      ),
-                      SizedBox(height: 10), // Adds some padding at the bottom
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
@@ -101,37 +125,95 @@ class MenuItem extends StatelessWidget {
   final Color color;
   final String text;
   final IconData icon;
+  final VoidCallback onTap;
 
-  MenuItem({required this.color, required this.text, required this.icon});
+  MenuItem({required this.color, required this.text, required this.icon, required this.onTap});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.symmetric(vertical: 10),
-      padding: EdgeInsets.all(20),
-      decoration: BoxDecoration(
-        color: color,
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Row(
-        children: [
-          Icon(
-            icon,
-            color: Colors.white,
-            size: 40,
-          ),
-          SizedBox(width: 20),
-          Expanded(
-            child: Text(
-              text,
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 10),
+        padding: EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Row(
+          children: [
+            Icon(
+              icon,
+              color: Colors.white,
+              size: 40,
+            ),
+            SizedBox(width: 20),
+            Expanded(
+              child: Text(
+                text,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class GreenScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        leading: BackButton(),
+      ),
+      body: Center(
+        child: Text(
+          '기숙사 입소 확인 페이지',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
+
+class OrangeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.orange,
+        leading: BackButton(),
+      ),
+      body: Center(
+        child: Text(
+          '기숙사 미입소 확인 페이지',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
+
+class BlueScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        leading: BackButton(),
+      ),
+      body: Center(
+        child: Text(
+          '공지사항 확인 페이지',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
       ),
     );
   }
